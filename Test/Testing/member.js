35e9e12f75d13aa3f9036aa067f2fc434305bd5b
Bryan Aristo Kenley <152769035+bryankenley@users.noreply.github.com>
2024-05-24 06:01:50 +0000

    Copilot second commit

---
+++ Test/Testing/member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Python', 'Django', 'Ruby', 'Ruby on Rails', 'Java', 'C++', 'C#', 'SQL', 'PHP', 'Swift', 'Objective-C', 'C', 'Cocoa', 'ActionScript', 'Perl', 'Lisp', 'Haskell', 'R', 'Matlab', 'Scala', 'Clojure', 'Groovy', 'Go', 'Erlang', 'Lua', 'Rust', 'Kotlin', 'TypeScript', 'CoffeeScript', 'Shell', 'PowerShell', 'Bash', 'VimL', 'Elixir', 'Visual Basic', 'Assembly', 'Scheme', 'Dart', 'F#', 'Racket', 'Eiffel', 'Turing', 'OCaml', 'Apex', 'LabVIEW', 'Verilog'];
+    var skillsList = skills.join(', ');
+    return skillsList;
+}
+
+console.log(skillsMember());
